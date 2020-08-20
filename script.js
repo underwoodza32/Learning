65cce573998ced155036108d7a108ee07b297608
underwoodza32 <55517884+underwoodza32@users.noreply.github.com>
2020-08-20 13:50:56 -0400

    new changes to java script file about arrow function

--- script.js
+++ script.js
@@ -65,3 +65,26 @@ const n = `${firstName} ${lastName}`;
 console.log(n.startsWith('J'));
 console.log(firstName.repeat(5));
 console.log(`${firstName} `.repeat(5));
+
+
+/////////////////////////////////////////////////////////////////////////////////
+// Lecture: Arrow Functions
+
+const years = [1990,1995,2000];
+
+// ES5
+var ages5 = years.map(function(el){
+    return 2016 - el;
+});
+console.log(ages5);
+
+// ES6
+
+let ages6 = years.map(el => 2016-el);
+console.log(ages6);
+
+ages6 = years.map((el, index)=> `Age element ${index + 1}: ${2016 - el}.`);
+console.log(ages6);
+
+/////////////////////////////////////////////////////////////////////////////////
+// Lecture: Arrow Functions 'This' keyword
